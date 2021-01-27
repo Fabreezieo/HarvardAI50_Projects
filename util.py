e25c2025831d52c53d4d7d216c5c5904acb71654
Fabreezieo <fabricol6@gmail.com>
2021-01-27 22:40:40 +0000

    first ai50/degrees commit

---
+++ util.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, actor_id, parent_actor, from_movie_id):
+        self.actor_id = actor_id
+        self.parent_actor = parent_actor
+        self.from_movie_id = from_movie_id
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, actor_id):
+        return any(node.actor_id == actor_id for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
